99554c16d1eee55c9de854444173eebcaf908ff8
Noah Strayve <noahstrayve@gmail.com>
2018-03-03 00:23:41 -0600

    final product before heroku

--- friends.js
+++ friends.js
@@ -1,5 +1,27 @@
-// Variable to hold the array of friends objects data
-var friends = [{ "name": "Charlie Kelly", "photo": "./images/CharlieFriendGood.png", "scores": ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"] }];
+var friends = [{ "name": "Charlie", 
+"photo": "https://pbs.twimg.com/media/CYTkNMPWYAAR99q.jpg", 
+"scores": ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"] 
+}, { "name": "Johnathon", 
+"photo": "https://pbs.twimg.com/media/BpdbQbrCUAEELiQ.jpg", 
+"scores": ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"] 
+}, { "name": "Craigory", 
+"photo": "https://images.megapixl.com/4314/43147266.jpg", 
+"scores": ["5", "3", "4", "2", "1", "4", "1", "3", "2", "4"] 
+}, { "name": "Vladimir", 
+"photo": "http://theawesomedaily.com/wp-content/uploads/2014/02/x29ZFsC-600x600.jpg", 
+"scores": ["1", "2", "3", "4", "5", "4", "3", "2", "1", "2"] 
+}, { "name": "Old Man", 
+"photo": "http://www.abc.net.au/news/image/4173092-3x2-940x627.jpg", 
+"scores": ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"] 
+}, { "name": "Grutumbo", 
+"photo": "https://sensemachine.files.wordpress.com/2011/09/most-muscular-pose.jpg", 
+"scores": ["2", "2", "1", "5", "5", "3", "5", "4", "1", "1"] 
+}, { "name": "Chad", 
+"photo": "https://pbs.twimg.com/media/BH7mQBoCIAAbNbo.jpg", 
+"scores": ["4", "4", "2", "4", "1", "1", "3", "2", "4", "4"] 
+}, { "name": "Vincienzo", 
+"photo": "https://i.pinimg.com/originals/ba/19/e7/ba19e75f81f15bdb1c2007f05e54a2e8.jpg", 
+"scores": ["2", "5", "5", "3", "3", "2", "2", "1", "4", "3"] 
+}];
 
-// Export the data
 module.exports = friends;
